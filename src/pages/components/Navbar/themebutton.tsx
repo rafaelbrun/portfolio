d541761286b045685e1f3d87bf379236d8caa104
Rafael Golin <rafaelbrungolin@gmail.com>
2023-08-23 21:44:28 +1000

    feat(ThemeButton): add ThemeButton

---
+++ src/pages/components/Navbar/themebutton.tsx
@@ -0,0 +1,30 @@
+import { useTheme } from "next-themes";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { FiMoon } from "react-icons/fi";
+import { BsSun } from "react-icons/bs";
+
+export default function ThemeButton() {
+  const { theme, setTheme } = useTheme();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const toggleTheme = useCallback(() => {
+    theme == "dark" ? setTheme("light") : setTheme("dark");
+  }, [setTheme, theme]);
+
+  const CurrentIcon = useMemo(() => {
+    return theme == "dark" ? (
+      <FiMoon onClick={toggleTheme} className="h-8 w-8 cursor-pointer" />
+    ) : (
+      <BsSun
+        onClick={toggleTheme}
+        className="h-8 w-8 cursor-pointer text-darkred"
+      />
+    );
+  }, [theme, toggleTheme]);
+
+  return isClient ? CurrentIcon : null;
+}
