d2a6bf57371d6f6d595017fbadcadb71ea1fe9d1
Rafael Golin <rafaelbrungolin@gmail.com>
2023-08-23 23:05:07 +1000

    feat(Navbar): implement responsivity

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,5 +1,6 @@
 import Navbutton from "./navbutton";
 import ThemeButton from "./themebutton";
+import { HiMenu } from "react-icons/hi";
 
 const TABS = ["Career", "Graduation", "Projects", "About"];
 
@@ -10,13 +11,18 @@ export default function Navbar() {
   };
 
   return (
-    <div className="fixed top-0 flex w-full items-center justify-end px-10 opacity-90">
-      {TABS.map((tab) => {
-        return (
-          <Navbutton title={tab} onClick={() => onClickTest(tab)} key={tab} />
-        );
-      })}
+    <div className="fixed top-0 flex w-full items-center justify-end px-8 opacity-90 sm:justify-center lg:justify-end lg:px-72">
+      <div className="hidden sm:flex">
+        {TABS.map((tab) => {
+          return (
+            <Navbutton title={tab} onClick={() => onClickTest(tab)} key={tab} />
+          );
+        })}
+      </div>
       <ThemeButton />
+      <div>
+        <HiMenu className="flex sm:hidden h-8 w-8 m-4 cursor-pointer text-darkred dark:text-white"/>
+      </div>
     </div>
   );
 }
