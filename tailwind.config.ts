32d56071017dd64c106c1a47df17ed75e9abbe41
Rafael Golin <rafaelbrungolin@gmail.com>
2023-08-23 18:20:33 +1000

    feat(Theme): add dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,13 @@ import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkred: { DEFAULT: "#1A0000" },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
