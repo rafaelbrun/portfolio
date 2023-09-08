ba839b834ab5d67f7c7f4a6c56bdb3fdccaf7cd2
Rafael Golin <rafaelbrungolin@gmail.com>
2023-09-08 10:20:22 +1000

    chore(Jest): add jest testing config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,12 @@ const config = {
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "testing-library"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:testing-library/react",
   ],
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
