747351ae91a8fb4dd80ed09b8f68999f438ef729
Rafael Golin <rafaelbrungolin@gmail.com>
2023-09-26 22:17:40 +1000

    chore(Tailwind): add wiggle animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ export default {
       colors: {
         darkred: { DEFAULT: "#120000" },
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-6deg)' },
+          '50%': { transform: 'rotate(6deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      }
     },
   },
   plugins: [],
