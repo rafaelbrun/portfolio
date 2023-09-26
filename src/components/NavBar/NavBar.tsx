5ae0b95294b3c24cafe986de3acaefd08d0c9df2
Rafael Golin <rafaelbrungolin@gmail.com>
2023-09-26 22:17:11 +1000

    refactor(NavBar): cleanup and adjustments

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -8,33 +8,32 @@ const TABS = ["Career", "Graduation", "Projects", "About"];
 const NAVBAR_DEFAULT =
   "fixed top-0 flex w-full items-center justify-end px-8 opacity-90 sm:justify-center lg:justify-end lg:px-72";
 const NAVBAR_MENU =
-  "fixed top-0 right-0 flex flex-col h-full px-8 opacity-90 sm:justify-center sm:right-1 sm:flex-row sm:w-full sm:h-auto lg:justify-end lg:px-72";
+  "fixed bg-red-950 top-0 right-0 flex flex-col h-full px-8 opacity-90 sm:justify-center sm:right-1 sm:flex-row sm:w-full sm:bg-transparent sm:h-auto lg:justify-end lg:px-72";
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const openMenu = useCallback(() => {
-    setIsMenuOpen(true);
+  const toggleMenu = useCallback(() => {
+    setIsMenuOpen((prevState) => !prevState);
   }, []);
 
-  const closeMenu = useCallback(() => {
-    setIsMenuOpen(false);
-  }, []);
-
-  const ListOfNavbuttons = useMemo(() => {
-    return TABS.map((tab) => <Navbutton title={tab} key={tab} />);
-  }, []);
+  const ListOfNavbuttons = useMemo(
+    () => TABS.map((tab) => <Navbutton title={tab} key={tab} />),
+    []
+  );
 
   return (
     <div className={isMenuOpen ? NAVBAR_MENU : NAVBAR_DEFAULT} role="menubar">
       <div className={"hidden sm:flex"}>{ListOfNavbuttons}</div>
       <div className="flex flex-row">
-        <ThemeButton />
+        <div className="mx-4">
+          <ThemeButton />
+        </div>
         <div className="m-4 flex cursor-pointer text-darkred dark:text-white sm:hidden">
           {!isMenuOpen ? (
-            <HiMenu onClick={openMenu} className="h-8 w-8" />
+            <HiMenu onClick={toggleMenu} className="h-8 w-8" />
           ) : (
-            <CgClose onClick={closeMenu} className="h-8 w-8" />
+            <CgClose onClick={toggleMenu} className="h-8 w-8" />
           )}
         </div>
       </div>
@@ -49,6 +48,6 @@ const NavBar = () => {
       </div>
     </div>
   );
-}
+};
 
 export default NavBar;
