9a028fe87512db798f463b0eb404ebbc932eaa0b
Rafael Golin <rafaelbrungolin@gmail.com>
2023-10-05 09:25:13 +1100

    feat(Section): add skills and contact

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -1,12 +1,12 @@
-const About = () => {
+const Contact = () => {
     return (
       <div className="mx-8 my-24 flex flex-col sm:mx-24">
         <b className="flex flex-row text-4xl tracking-widest text-darkred dark:text-white lg:text-6xl">
-          ABOUT
+          CONTACT
         </b>
       </div>
     );
   };
   
-  export default About;
+  export default Contact;
   
