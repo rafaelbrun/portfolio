12461f932902872760e14ba1e102704d3b5002c9
Rafael Golin <rafaelbrungolin@gmail.com>
2023-10-05 09:06:27 +1100

    feat(Sections): create sections placeholder

---
+++ src/components/Sections/Career.tsx
@@ -0,0 +1,11 @@
+const Career = () => {
+  return (
+    <div className="mx-8 my-24 flex flex-col sm:mx-24">
+      <b className="flex flex-row text-4xl tracking-widest text-darkred dark:text-white lg:text-6xl">
+        CAREER
+      </b>
+    </div>
+  );
+};
+
+export default Career;
