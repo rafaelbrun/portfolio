630894657bed41aae3f2262eaaa7549f4e917860
Rafael Golin <rafaelbrungolin@gmail.com>
2023-10-05 09:09:28 +1100

    feat(Section): adjust Section layout

--- src/components/NavBar/NavButton.tsx
+++ src/components/NavBar/NavButton.tsx
@@ -1,16 +1,20 @@
 import type { NavbuttonProps } from "~/interfaces/NavButton";
+import { Tabs } from "../Sections/types";
+import Link from "next/link";
 
 const NavButton = ({ title }: NavbuttonProps) => {
   const ref = `#${title.toLowerCase()}`;
 
+  const isMain = title === Tabs.MAIN;
+
   return (
     <div className="group flex flex-col self-end">
-      <a
+      <Link
         href={ref}
         className="self-end tracking-wider text-darkred dark:text-white sm:text-start"
       >
-        {title}
-      </a>
+        {isMain ? "RG" : title}
+      </Link>
       <span className="h-0.25 max-w-0 transform bg-darkred transition-all group-hover:max-w-full dark:bg-white" />
     </div>
   );
