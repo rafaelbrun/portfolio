6909b48f6493d094b896cf9ee9f6c53d7059b5f0
Rafael Golin <rafaelbrungolin@gmail.com>
2024-03-14 15:01:56 +1000

    feat(Fonts): add font

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,13 @@
+import { exo2 } from "~/utils/fonts";
 import Navbar from "./NavBar/NavBar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className={exo2.className}>
       <Navbar />
       <div className="min-h-screen pt-16">{children}</div>
-    </>
+    </div>
   );
-}
+};
 
 export default Layout;
