b951cbcf48e4e79800ea32a1d182cf9601dc7fe9
Rafael Golin <rafaelbrungolin@gmail.com>
2024-03-14 15:02:23 +1000

    feat(MainSection): add dev symbol

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,17 +9,20 @@ export default {
         darkred: { DEFAULT: "#120000" },
       },
       spacing: {
-        '0.25': '0.063rem',
+        "0.25": "0.063rem",
       },
       keyframes: {
         wiggle: {
-          '0%, 100%': { transform: 'rotate(-6deg)' },
-          '50%': { transform: 'rotate(6deg)' },
-        }
+          "0%, 100%": { transform: "rotate(-6deg)" },
+          "50%": { transform: "rotate(6deg)" },
+        },
       },
       animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
-      }
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
+      fontSize: {
+        xxl: "12rem",
+      },
     },
   },
   plugins: [],
