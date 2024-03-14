59116210a31192b731d0cb3a5b537207f09ac7aa
Rafael Golin <rafaelbrungolin@gmail.com>
2024-03-14 13:17:39 +1000

    fix(Navbar): responsivity

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,11 +6,11 @@ import { CgClose } from "react-icons/cg";
 import { SECTIONS } from "../Sections/types";
 
 const NAVBAR_DEFAULT =
-  "fixed z-50 backdrop-filter backdrop-blur dark:bg-darkred/20 top-0 flex w-full items-center justify-end px-8 " +
+  "fixed z-50 backdrop-filter backdrop-blur dark:bg-darkred/20 top-0 flex w-full items-center justify-end px-4 " +
   "sm:justify-center lg:justify-end lg:px-72";
 const NAVBAR_MENU =
   "fixed bg-red-400/50 dark:bg-red-950/50 backdrop-filter backdrop-blur sm:bg-white sm:dark:bg-transparent top-0 " +
-  "right-0 flex items-end flex-col h-full px-8 sm:items-center sm:z-50 sm:justify-center sm:right-1 sm:flex-row " +
+  "right-0 flex items-end flex-col h-full px-4 sm:items-center z-50 sm:justify-center sm:right-1 sm:flex-row " +
   "sm:w-full sm:bg-white/20 sm:h-auto lg:justify-end lg:px-72";
 
 const NavBar = () => {
@@ -37,9 +37,9 @@ const NavBar = () => {
         </div>
         <div className="text-darkred-3xl my-4 flex cursor-pointer dark:text-white sm:hidden">
           {!isMenuOpen ? (
-            <HiMenu onClick={toggleMenu} className="h-6 w-6" />
+            <HiMenu onClick={toggleMenu} className="h-4 w-4 sm:h-6 sm:w-6" />
           ) : (
-            <CgClose onClick={toggleMenu} className="h-6 w-6" />
+            <CgClose onClick={toggleMenu} className="h-4 w-4 sm:h-6 sm:w-6" />
           )}
         </div>
       </div>
