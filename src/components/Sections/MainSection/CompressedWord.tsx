1c1c2ec90a991e6341b4457e67bba598b81d5613
Rafael Golin <rafaelbrungolin@gmail.com>
2024-03-14 13:17:06 +1000

    fix(Sections): adjust folder

---
+++ src/components/Sections/MainSection/CompressedWord.tsx
@@ -0,0 +1,20 @@
+const CompressedWord = ({ word }: { word: string }) => {
+  const firstLetter = word.split("")[0];
+  const restOfWord = word.split("").slice(1).join("");
+
+  return (
+    <div className="group mx-2 flex max-w-fit text-red-800 sm:mx-4 lg:mx-6">
+      <b className="animate-wiggle text-xl group-hover:animate-none sm:text-4xl lg:text-6xl">
+        {firstLetter}
+      </b>
+      <b className="text-xl group-hover:opacity-0 group-hover:transition-opacity sm:text-4xl lg:text-6xl">
+        .
+      </b>
+      <b className="-ms-1 w-0 -translate-x-8 transform text-xl tracking-widest opacity-0 transition-all group-hover:w-12 group-hover:translate-x-0 group-hover:opacity-100 sm:text-4xl lg:text-6xl lg:group-hover:w-36">
+        {restOfWord}
+      </b>
+    </div>
+  );
+};
+
+export default CompressedWord;
