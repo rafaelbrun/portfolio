0929e01f474201137665a3680c5cf38e9a0a8e04
Rafael Golin <rafaelbrungolin@gmail.com>
2024-04-08 11:32:21 +1000

    fix(Text): adjust text size responsivity

--- src/components/Sections/Skills.tsx
+++ src/components/Sections/Skills.tsx
@@ -1,25 +1,27 @@
 const Skills = () => {
   return (
     <div>
-      <ul>
+      <ul className="text-sm md:text-base">
         <li>
-          <b className="text-lg">Programming Languages:</b> JavaScript,
-          TypeScript, Kotlin for Android (XML and JetpackCompose), Java and SQL.
+          <b className="text-base md:text-lg">Programming Languages:</b>{" "}
+          JavaScript, TypeScript, Kotlin for Android (XML and JetpackCompose),
+          Java and SQL.
         </li>
         <li>
-          <b className="text-lg">Frameworks:</b> React, Nextjs, React Native,
-          Angular, Spring Boot and NodeJS.
+          <b className="text-base md:text-lg">Frameworks:</b> React, Nextjs,
+          React Native, Angular, Spring Boot and NodeJS.
         </li>
         <li>
-          <b className="text-lg">Tools:</b> Git, Swagger, Jenkins, Postman,
-          Charles, Flipper, Apollo, Firebase, Sentry and Fastlane.
+          <b className="text-base md:text-lg">Tools:</b> Git, Swagger, Jenkins,
+          Postman, Charles, Flipper, Apollo, Firebase, Sentry and Fastlane.
         </li>
         <li>
-          <b className="text-lg">State Management:</b> Redux, React Query,
-          Recoil and MutableState.
+          <b className="text-base md:text-lg">State Management:</b> Redux, React
+          Query, Recoil and MutableState.
         </li>
         <li>
-          <b className="text-lg">Experience with</b> RESTful APIs and GraphQL.
+          <b className="text-base md:text-lg">Experience with</b> RESTful APIs
+          and GraphQL.
         </li>
       </ul>
     </div>
